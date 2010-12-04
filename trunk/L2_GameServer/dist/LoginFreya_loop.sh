93ad29f555cdb12443e4e2d0d1f3d2ccd287aaba
Melua <bellevant@melua.fr>
2010-12-04 11:17:15 +0000

    activation de l'utf-8 dans la JVM

--- trunk/L2_GameServer/dist/LoginFreya_loop.sh
+++ trunk/L2_GameServer/dist/LoginFreya_loop.sh
@@ -6,7 +6,7 @@ do
 	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
 	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
 	#nice -n -2 java -Xms128m -Xmx128m -cp ./../libs/*:l2jlogin.jar com.l2jserver.loginserver.L2LoginServer > log/stdout.log 2>&1
-    java -Xms128m -Xmx128m -cp ./../libs/*:l2jlogin.jar com.l2jserver.loginserver.L2LoginServer > log/stdout.log 2>&1
+    java -Dfile.encoding=UTF-8 -Xms128m -Xmx128m -cp ./../libs/*:l2jlogin.jar com.l2jserver.loginserver.L2LoginServer > log/stdout.log 2>&1
 	err=$?
 #	/etc/init.d/mysql restart
 	sleep 10;
