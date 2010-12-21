a8d309b93c90e54a4c89d8b21e1f9d89cceefac0
Melua <bellevant@melua.fr>
2010-12-21 22:09:18 +0000

    fixfix AdminNpcSay (substring au lieu de tokenizer)

--- datapack_development/data/scripts/handlers/admincommandhandlers/AdminNpcSay.java
+++ datapack_development/data/scripts/handlers/admincommandhandlers/AdminNpcSay.java
@@ -5,7 +5,6 @@ import com.l2jserver.gameserver.handler.IAdminCommandHandler;
 import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.network.clientpackets.Say2;
 import com.l2jserver.gameserver.network.serverpackets.CreatureSay;
-import java.util.StringTokenizer;
 
 /**
  * @author Melua
@@ -18,17 +17,15 @@ public class AdminNpcSay implements IAdminCommandHandler {
     public boolean useAdminCommand(String command, L2PcInstance activeChar) {
 
         if (activeChar.getTarget() != null) {
-            if (activeChar.getTarget() instanceof L2PcInstance) {
-                activeChar.sendMessage("Il n'est pas permis de faire parler un joueur.");
-            } else if (activeChar.getTarget() instanceof L2Npc) {
-                StringTokenizer st = new StringTokenizer(command, " ");
-                st.nextToken();
-                if (st.hasMoreTokens()) {
+            if (activeChar.getTarget() instanceof L2Npc) {
+                if (command.length() > 9) {
                     L2Npc npc = (L2Npc) activeChar.getTarget();
-                    npc.broadcastPacket(new CreatureSay(npc.getObjectId(), Say2.ALL, npc.getName(), st.nextToken()));
+                    npc.broadcastPacket(new CreatureSay(npc.getObjectId(), Say2.ALL, npc.getName(), command.substring(9)));
                 } else {
                     activeChar.sendMessage("Usage : //>> texte roleplay");
                 }
+            } else if (activeChar.getTarget() instanceof L2PcInstance) {
+                activeChar.sendMessage("Il n'est pas permis de faire parler un joueur.");
             }
         } else {
             activeChar.sendMessage("Vous n'avez pas de cible à faire parler.");
