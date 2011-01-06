0372e4be597061dc4b62550cc6e40569bb00f06d
Melua <bellevant@melua.fr>
2011-01-06 09:26:52 +0000

    fix mail (à tester)

--- L2_GameServer/java/com/l2jserver/gameserver/network/clientpackets/RequestPostItemList.java
+++ L2_GameServer/java/com/l2jserver/gameserver/network/clientpackets/RequestPostItemList.java
@@ -14,7 +14,7 @@
  */
 package com.l2jserver.gameserver.network.clientpackets;
 
-import static com.l2jserver.gameserver.model.actor.L2Character.ZONE_PEACE;
+import static com.l2jserver.gameserver.model.actor.L2Character.ZONE_TOWN;
 
 import com.l2jserver.Config;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
@@ -45,10 +45,11 @@ public final class RequestPostItemList extends L2GameClientPacket
 		if (activeChar == null)
 			return;
 		
-		if (!activeChar.isInsideZone(ZONE_PEACE))
+		if (!activeChar.isInsideZone(ZONE_TOWN))
 		{
-			activeChar.sendPacket(new SystemMessage(SystemMessageId.CANT_USE_MAIL_OUTSIDE_PEACE_ZONE));
-			return;
+			//activeChar.sendPacket(new SystemMessage(SystemMessageId.CANT_USE_MAIL_OUTSIDE_PEACE_ZONE));
+                        activeChar.sendMessage("Vous ne pouvez pas utiliser les mails en dehors des villes.");
+                    return;
 		}
 		
 		activeChar.sendPacket(new ExReplyPostItemList(activeChar));
