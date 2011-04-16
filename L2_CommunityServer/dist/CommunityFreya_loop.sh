1cbb4a43ae4af2a75a2f2d8a20fac887b0dca850
Melua <bellevant@melua.fr>
2011-04-16 00:04:17 +0000

    essai de perf avec l'option de JVM Server (ne pas tester en production!!!)

--- L2_CommunityServer/dist/CommunityFreya_loop.sh
+++ L2_CommunityServer/dist/CommunityFreya_loop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while :; do
-
-	java -Dfile.encoding=UTF-8 -Xms128m -Xmx128m -cp ./../libs/*:l2jcommunity.jar com.l2jserver.communityserver.L2CommunityServer > log/stdout.log 2>&1
+    # -server plus optimisé mais néccessite le JDK
+	java -server -Dfile.encoding=UTF-8 -Xms128m -Xmx128m -cp ./../libs/*:l2jcommunity.jar com.l2jserver.communityserver.L2CommunityServer > log/stdout.log 2>&1
 	sleep 10
 done
--- L2_CommunityServer/dist/startCommunityServer.sh
+++ L2_CommunityServer/dist/startCommunityServer.sh
@@ -1,3 +1,9 @@
-#! /bin/sh
+#!/bin/bash
 
-./CommunityFreya_loop.sh &
+if [[ $EUID -ne 1001 ]];
+then
+   echo "Ce script doit être lancé avec l'utilisateur VaeSoli !" 1>&2
+   exit 1
+else
+   ./CommunityFreya_loop.sh &
+fi
