39e5e5923735a773a289832e5a7c11c71e89d269
Melua <bellevant@melua.fr>
2011-06-15 20:48:27 +0000

    dev sur la siege penalty

--- datapack_development/data/scripts/handlers/admincommandhandlers/AdminPenalty.java
+++ datapack_development/data/scripts/handlers/admincommandhandlers/AdminPenalty.java
@@ -17,22 +17,25 @@ public class AdminPenalty implements IAdminCommandHandler {
 
     public boolean useAdminCommand(String command, L2PcInstance activeChar) {
         StringTokenizer st = new StringTokenizer(command, " ");
+        st.nextToken();
 
         if (command.startsWith("admin_penalty")) {
             try {
                 int val = Integer.parseInt(st.nextToken());
+                boolean sendMessage = activeChar.getFirstEffect(7029) != null;
                 if (activeChar.getTarget() instanceof L2PcInstance) {
-                    L2PcInstance player = (L2PcInstance) activeChar.getTarget();
-                    player.stopSkillEffects(5660);
-                    if (val == 0 && player.getFirstEffect(5660) != null) {
-                        player.sendPacket(new SystemMessage(SystemMessageId.EFFECT_S1_DISAPPEARED).addSkillName(5660));
-                    } else if ((val >= 1) && (val <= 5)) {
-                        L2Skill deathPenaltySkill = SkillTable.getInstance().getInfo(5660, val);
-                        player.doSimultaneousCast(deathPenaltySkill);
-                    }
+                L2PcInstance player = (L2PcInstance) activeChar.getTarget();
+                player.stopSkillEffects(5660);
+                if (val == 0 && sendMessage) {
+                    player.sendPacket(new SystemMessage(SystemMessageId.EFFECT_S1_DISAPPEARED).addSkillName(7029));
+                } else if ((val >= 1) && (val <= 5)) {
+                    L2Skill deathPenaltySkill = SkillTable.getInstance().getInfo(5660, val);
+                    deathPenaltySkill.getEffects(player, player);
+                } else {
+                    activeChar.sendMessage("Usage: //penalty <value> (0=off...5=max)");
+                }
                 }
             } catch (Exception e) {
-                activeChar.sendMessage("Usage: //penalty <value> (0=off...5=max)");
             }
         }
         return true;
