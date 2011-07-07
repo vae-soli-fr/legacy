4c2dd18ca6b9b9f057cc4b3dcee181fd5a4e2f81
Melua <bellevant@melua.fr>
2011-07-07 17:33:54 +0000

    temperatures (0-> -30)

---
+++ L2_GameServer/java/com/l2jserver/gameserver/vaesoli/Market.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.l2jserver.gameserver.vaesoli;
+
+/**
+ *
+ * @author Kevin
+ */
+public class Market {
+
+}
