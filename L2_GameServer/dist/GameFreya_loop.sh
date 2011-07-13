f3dcec0da8b64c052cb70b02172ccbe812fae8aa
Melua <bellevant@melua.fr>
2011-07-13 21:27:36 +0000

    version à la compilation

--- L2_GameServer/dist/GameFreya_loop.sh
+++ L2_GameServer/dist/GameFreya_loop.sh
@@ -8,6 +8,12 @@ while :; do
     if test -f "updater/l2jserver.jar"; then
       cp "l2jserver.jar" "l2jserver.`date +%d-%m-%Y_%H-%M`.bak"
       mv "updater/l2jserver.jar" "l2jserver.jar"
+        if test -f "updater/l2j-version.properties"; then
+        mv -f "updater/l2j-version.properties" "config/l2j-version.properties"
+        fi
+    fi
+    if test -f "updater/l2jdp-version.properties"; then
+      mv -f "updater/l2jdp-version.properties" "config/l2jdp-version.properties"
     fi
 	[ -f log/java0.log.0 ] && mv log/java0.log.0 "log/`date +%Y-%m-%d_%H-%M-%S`_java.log"
 	[ -f log/stdout.log ] && mv log/stdout.log "log/`date +%Y-%m-%d_%H-%M-%S`_stdout.log"
