a9a27642c0352dcd515846dc27b7d64509298705
Melua <bellevant@melua.fr>
2011-07-17 22:45:25 +0000

    certif échange (terminé)

--- datapack_development/data/scripts/custom/CertificationEx/CertificationEx.java
+++ datapack_development/data/scripts/custom/CertificationEx/CertificationEx.java
@@ -6,6 +6,9 @@ import com.l2jserver.gameserver.model.actor.L2Npc;
 import com.l2jserver.gameserver.model.actor.instance.L2PcInstance;
 import com.l2jserver.gameserver.model.quest.Quest;
 import com.l2jserver.gameserver.model.quest.QuestState;
+import com.l2jserver.gameserver.network.SystemMessageId;
+import com.l2jserver.gameserver.network.serverpackets.StatusUpdate;
+import com.l2jserver.gameserver.network.serverpackets.SystemMessage;
 import com.l2jserver.gameserver.util.Util;
 import java.util.StringTokenizer;
 
@@ -31,14 +34,12 @@ public class CertificationEx extends Quest {
         super(id, name, descr);
         addStartNpc(_npc);
         addTalkId(_npc);
-        addFirstTalkId(_npc);
     }
 
-	@Override
-	public String onFirstTalk(L2Npc npc, L2PcInstance player)
-	{
-		return "main.htm";
-	}
+    @Override
+    public String onTalk(L2Npc npc, L2PcInstance player) {
+        return "main.htm";
+    }
 
     @Override
     public String onAdvEvent(String event, L2Npc npc, L2PcInstance player) {
@@ -46,22 +47,20 @@ public class CertificationEx extends Quest {
         // doit être en main class
         if (player.getClassIndex() != 0) {
             return "class.htm";
-        }
-
-        // quelle deuxième page afficher
+        } // quelle deuxième page afficher
         else if (event.startsWith("ask")) {
-            if (Util.contains(KNIGHTCLASSES, player.getClassId().getId()))
+            if (Util.contains(KNIGHTCLASSES, player.getClassId().getId())) {
                 return "knightlist.htm";
-            else if (Util.contains(KNIGHTCLASSES, player.getClassId().getId()))
+            } else if (Util.contains(SUMMONERCLASSES, player.getClassId().getId())) {
                 return "summonlist.htm";
-            else
+            } else {
                 return "defaultlist.htm";
-        }
-
-        // effectuer l'échange
+            }
+        } // effectuer l'échange
         else if (event.startsWith("change")) {
 
-            StringTokenizer st = new StringTokenizer(event);
+            StringTokenizer st = new StringTokenizer(event, " ");
+            st.nextToken();
             int itemId = Integer.parseInt(st.nextToken());
             int rewardId = Integer.parseInt(st.nextToken());
 
@@ -76,29 +75,47 @@ public class CertificationEx extends Quest {
             QuestState qt = player.getQuestState(CertificationEx.class.getSimpleName());
             String qName, qValue;
             int objectId, certId;
-                for (int j = Config.MAX_SUBCLASS; j > 0; j--) {
-                    qName = "ClassAbility75-" + String.valueOf(j);
-                    qValue = qt.getGlobalQuestVar(qName);
+            for (int j = Config.MAX_SUBCLASS; j > 0; j--) {
+                qName = "ClassAbility75-" + String.valueOf(j);
+                qValue = qt.getGlobalQuestVar(qName);
+                if (qValue == null || qValue.isEmpty()) {
+                    continue;
+                }
 
-                    if (!qValue.endsWith(";")) // not a skill
+                if (!qValue.endsWith(";")) // not a skill
+                {
+                    objectId = Integer.parseInt(qValue);
+                    if (objectId == 0) // canceled skill, no item
                     {
-                        objectId = Integer.parseInt(qValue);
-                        certItem = player.getInventory().getItemByObjectId(objectId);
-                        if (certItem != null) {
-                            certId = certItem.getItemId();
-                            if (certId == itemId && Util.contains(SKILLITEMS, certId)) {
-                                qt.takeItems(57, 2000000);
-                                qt.takeItems(certId, 1);
-                                rewardItem = player.getInventory().addItem("CertificationEx", rewardId, 1, player, player.getTarget());
-                                qt.saveGlobalQuestVar(qName, Integer.toString(rewardItem.getObjectId()));
-                                return null;
-                            }
-                        }
+                        continue;
                     }
 
+                    if (player.getInventory().getInventoryItemCount(5575, 0) < 2000000) // not enough AA
+                    {
+                        continue;
+                    }
+
+                    certItem = player.getInventory().getItemByObjectId(objectId);
+                    if (certItem != null) {
+                        certId = certItem.getItemId();
+                        if (certId == itemId && Util.contains(SKILLITEMS, certId)) {
+                            qt.takeItems(5575, 2000000);
+                            qt.takeItems(certId, 1);
+                            rewardItem = player.getInventory().addItem("CertificationEx", rewardId, 1, player, player.getTarget());
+                            qt.saveGlobalQuestVar(qName, Integer.toString(rewardItem.getObjectId()));
+                            StatusUpdate su = new StatusUpdate(player);
+                            su.addAttribute(StatusUpdate.CUR_LOAD, player.getCurrentLoad());
+                            player.sendPacket(su);
+                            player.sendPacket(new SystemMessage(SystemMessageId.EARNED_ITEM).addItemName(rewardItem));
+                            return null;
+                        }
+                    }
                 }
+
+            }
+            return "error.htm";
         }
-        return "defaultlist.html";
+        return "main.htm";
 
     }
 
