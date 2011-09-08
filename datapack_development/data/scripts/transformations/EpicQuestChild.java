974a2aeb222d947ecf6c46bbe322b99f6f610039
Melua <bellevant@melua.fr>
2011-09-08 21:47:07 +0000

    transfo enfants (add dispel)

--- datapack_development/data/scripts/transformations/EpicQuestChild.java
+++ datapack_development/data/scripts/transformations/EpicQuestChild.java
@@ -4,9 +4,13 @@ import com.l2jserver.gameserver.datatables.SkillTable;
 import com.l2jserver.gameserver.instancemanager.TransformationManager;
 import com.l2jserver.gameserver.model.L2Transformation;
 
+/**
+ * La transformation est aussi utilisée en RP par les joueurs
+ */
+
 public class EpicQuestChild extends L2Transformation
 {
-	private static final int[] SKILLS = { 5437, 960 };
+	private static final int[] SKILLS = { 5437, 960, 619 };
 	
 	public EpicQuestChild()
 	{
@@ -29,6 +33,8 @@ public class EpicQuestChild extends L2Transformation
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(5437, 1), false);
 		// Race Running
 		getPlayer().addSkill(SkillTable.getInstance().getInfo(960, 1), false);
+                // Transform Dispel
+		getPlayer().addSkill(SkillTable.getInstance().getInfo(619, 1), false);
 		
 		getPlayer().setTransformAllowedSkills(SKILLS);
 	}
@@ -45,6 +51,8 @@ public class EpicQuestChild extends L2Transformation
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(5437, 1), false);
 		// Race Running
 		getPlayer().removeSkill(SkillTable.getInstance().getInfo(960, 1), false);
+                // Transform Dispel
+		getPlayer().removeSkill(SkillTable.getInstance().getInfo(619, 1), false);
 		
 		getPlayer().setTransformAllowedSkills(EMPTY_ARRAY);
 	}
