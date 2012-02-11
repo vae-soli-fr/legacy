fbbc34fc4d64d112d8d159508caf4da56ea9e589
Melua <bellevant@melua.fr>
2012-02-11 14:01:27 +0000

    SoA non aggro

---
+++ L2J_DataPack/dist/sql/vaesoli/updates/20120211update.sql
@@ -0,0 +1,4 @@
+-- Passive Seed of Annihilation
+UPDATE npcaidata SET `aggro` = 0 WHERE `npcId` BETWEEN 22754 AND 22759;
+UPDATE npcaidata SET `aggro` = 0 WHERE `npcId` BETWEEN 22760 AND 22765;
+UPDATE npcaidata SET `aggro` = 0 WHERE `npcId` BETWEEN 22746 AND 22753;
