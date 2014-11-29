db5a4f2e18993c62744b2b5935617dff1f08ebfc
Melua <bellevant@melua.fr>
2014-11-29 10:42:53 +0100

    Change path to libs

--- dist/gameserver/GameServer_loop.sh
+++ dist/gameserver/GameServer_loop.sh
@@ -8,12 +8,9 @@ while :; do
 	if test -f "updater/lineage2-gameserver.jar"; then
 		cp "../libs/lineage2-gameserver.jar" "../libs/lineage2-gameserver.`date +%d-%m-%Y_%H-%M`.bak"
 		mv "updater/lineage2-gameserver.jar" "../libs/lineage2-gameserver.jar"
-			if test -f "updater/lineage2-version.ini"; then
-			mv -f "updater/lineage2-version.ini" "config/lineage2-version.ini"
-			fi
 	fi
 	# -server plus optimisé
-	java -server -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 -Xms8192m -Xmx8192m -cp config/xml:../libs/*: lineage2.gameserver.GameServer > log/stdout.log 2>&1
+	java -server -Djava.net.preferIPv4Stack=true -Dfile.encoding=UTF-8 -Xms8192m -Xmx8192m -cp config/xml:libs/*: lineage2.gameserver.GameServer > log/stdout.log 2>&1
 	[ $? -ne 2 ] && break
 	sleep 30;
 done
